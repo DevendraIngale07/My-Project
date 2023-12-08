4d0876a28a90764e6bfd31398bf6b7a7edf76caa
DevendraIngale07 <devendraingale55@gmail.com>
2023-12-08 14:03:48 +0530

    update

---
+++ Week1/5-12/index.js
@@ -0,0 +1,34 @@
+
+// let words = "My name is Devendra narendra ingale".split(' ');
+// let [a,b,, ...d] = words;
+
+//   console.log(d); 
+
+
+// let nums = [12,56,78,87,98,55];
+
+// nums.forEach((n, i, nums) => {
+//     console.log((n, i, nums));
+// });
+ 
+  
+// let nums = [1,2,3,4,5,6,3,7,8];
+
+// let result = nums.filter( n => n%2 ===0)
+//     .map( n => n*2)
+//     .reduce((a,b) => a+b);
+
+//     console.log(result);
+ 
+
+// let nums = new Set();
+
+// nums.add(3)
+// nums.add(4)
+// nums.add(5)
+// nums.add("devendra")
+// nums.add("raj")
+// nums.add('abhishek')
+
+
+//  console.log(nums.has("raj"));
